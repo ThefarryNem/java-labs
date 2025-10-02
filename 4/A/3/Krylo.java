0e8490e4090ab6598b32876fc70a0385bbb0237a
ThefarryNem <petrovoleg645@gmail.com>
2025-10-02 21:04:51 +0300

    а хз что тут сделал

--- 4/A/3/Krylo.java
+++ 4/A/3/Krylo.java
@@ -93,17 +93,3 @@ class Samolet {
         return Objects.hash(marshrut, krylo);
     }
 }
-
-// Тест
-public class Main {
-    public static void main(String[] args) {
-        Krylo krylo1 = new Krylo(35.8, 122.4);
-        Samolet boeing = new Samolet(krylo1);
-
-        boeing.zadatMarshrut("Москва - Нью-Йорк");
-        boeing.vyvestiMarshrut();
-        boeing.letat();
-
-        System.out.println(boeing);
-    }
-}
